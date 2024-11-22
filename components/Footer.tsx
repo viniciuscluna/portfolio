3f1b3392d6e7d48382453492668998cb00e401c1
Vinícius Luna <36234150+viniciuscluna@users.noreply.github.com>
2024-11-21 21:38:05 -0300

    Tirar ponto

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => (
 
 <footer className="bg-white rounded-lg shadow dark:bg-gray-800">
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} <a href="https://flowbite.com/" className="hover:underline">Vinicius Luna</a>.
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} <a href="https://flowbite.com/" className="hover:underline">Vinicius Luna</a>
     </span>
     </div>
 </footer>
